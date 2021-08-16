fcc3c92797a43c40c4387a28363be57fc6ba517f
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-08-16 20:53:59 +0300

    added solution task 20

---
+++ Unit 5/src/GreatestCommonDivisor.java
@@ -0,0 +1,14 @@
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first<10 || second <10) {
+            return -1;
+        }
+        int answer = 0;
+        for (int i = 1; i<=first && i<=second; i++) {
+            if (first%i==0 && second%i==0) {
+                answer=i;
+            }
+        }
+        return answer;
+    }
+}
